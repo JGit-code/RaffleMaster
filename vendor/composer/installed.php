2f9600f56ce4922bcc9830953605228b034430be
JGit-code <jasongaud18@gmail.com>
2025-04-22 20:20:21 +0200

    Jason
    Jason

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'administrator/raffle-master',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e892e3587cf688d1e9c077429c1d21973fd949f7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'administrator/raffle-master' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e892e3587cf688d1e9c077429c1d21973fd949f7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
